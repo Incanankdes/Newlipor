9a3948166adc4b1ecaccbd8ceea59cc24cc1251f
Incanankdes <148406967+Incanankdes@users.noreply.github.com>
2023-11-08 14:30:01 +0800

    Add files via upload

--- tt.c
+++ tt.c
@@ -2,8 +2,21 @@
 
 int main()
 {
-    int a;
-    scanf("%d",&a);
-    printf("%d",a % 3);
+    int a,b/*,c*/;
+    int i = 1;
+    int c;
+    /*c = */scanf("%d %d",&a, &b);
+    
+    /*if(c = -1)
+    printf("因为程序错误，你即将被十个周男轮*");*/
+    for(i = 1;i <= b;i++)
+    {
+        if(a % i == 0 && b % i ==0)
+        c = i;
+    }
+    
+    printf("%d",c);
+
+    A:
     return 0;
 }
