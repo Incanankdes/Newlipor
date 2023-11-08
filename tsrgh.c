8f7f6f7bb793c9227d21d46e6d34a9c2a61a2a76
Incanankdes <148406967+Incanankdes@users.noreply.github.com>
2023-11-08 19:19:01 +0800

    Add files via upload

--- tsrgh.c
+++ tsrgh.c
@@ -3,29 +3,68 @@
 
 int main()
 {
-   int a, b, c;
-    int d=0;
-    int e=0;
-    int f=0; 
-    scanf("%d %d", &a, &b); 
-    int arr[a][b];     
-    while(scanf("%d",&c) != EOF)
+
+  int a, b, c;
+  int d = 0;
+  int e = 0;
+  int f = 0;
+  scanf("%d %d", &a, &b);
+  int w[a + b];
+  int v[a + b];
+
+  while (scanf("%d", &c) != 0)
+  {
+
+    w[d] = c;
+    d++;
+  }
+
+  /*if (a < b)
+  {
+    c = a;
+    a = b;
+    b = c;
+  }*/
+
+  while (2*e <= a + b)
+  {
+    if (e < a - 1)
     {
-      arr[d][e]=c;
-      e++;
-      if(e == b)
+      if (w[e] >= w[e + a] && w[e + a] <= w[e + 1])
       {
-        d++;
-        e=0;
-      }  
-    } 
-    for(c=0;c<b;c++)
+        v[f] = w[e + a];
+        v[f + 1] = w[e];
+      }
+      if (w[e] <= w[e + a] && w[e + a] <= w[e + 1])
+      {
+        v[f] = w[e];
+        v[f + 1] = w[e + a];
+      }
+      if (w[e] <= w[e + a] && w[e + a] >= w[e + 1])
+      {
+        v[f] = w[e];
+        v[f + 1] = w[e + 1];
+        w[e + 1] = w[e + a];
+      }
+    }
+    if (e == a - 1)
     {
-        for(f=0;f<a;f++)
-        {
-            printf("%d ",arr[f][c]);
-        }
-        printf("\n");
-    }   
-    return 0;
+      v[f] = (w[e] < w[e + a] ? w[e] : w[e + a]);
+      v[f + 1] = (w[e] > w[e + a] ? w[e] : w[e + a]);
+    }
+    if (e >= a)
+    {
+      v[f] = w[f];
+      v[f + 1] = w[f + 1];
+    }
+    e += 1;
+    f += 2;
+  }
+
+  for (e = 0; e < a + b; e++)
+  {
+    printf("%d ", v[e]);
+  }
+
+  return 0;
 }
